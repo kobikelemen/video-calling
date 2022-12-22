9432ce3336316098bf14b69614c622585118caeb
kobikelemen <kobi.kelemen20@gmail.com>
2022-12-22 23:36:32 +0000

    creating packets on the send and receive side working

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,11 @@ use std::sync::mpsc;
 fn main() {
     use cpal::traits::{DeviceTrait, HostTrait};
     let host = cpal::default_host();
-    let (playpacket_tx, playpacket_rx) = mpsc::channel();
-    let mut aud = audio::Audio::new(&host, playpacket_rx);
+    let (speaker_packet_tx, speaker_packet_rx) = mpsc::channel();
+    let (mic_packet_tx, mic_packet_rx) = mpsc::channel();
+    let mut aud = audio::Audio::new(&host, speaker_packet_rx, mic_packet_tx);
     let server_connection = client::ServerConnection::new();
-    let friend_name : String= "jeff".to_string();
+    let friend_name : String = "jeff".to_string();
     let port = 1069; // recv from port
     let call_connection = client::CallConnection::new(friend_name, server_connection, port);
 
@@ -20,11 +21,14 @@ fn main() {
             Some(audiopacket) => {
                 // send the packets samples to the sound playing thread using a pipe, 
                 //      the thread then adds it into a queue which it plays samples from
-                playpacket_tx.send(audiopacket);
+                speaker_packet_tx.send(audiopacket);
             },
             None => {
 
             },
+        }
+        if let Ok(audio_packet) = mic_packet_rx.try_recv() {
+            call_connection.send_data(audio_packet)
         }
         // get sound samples from mic and put into packet
         // send packet over network
